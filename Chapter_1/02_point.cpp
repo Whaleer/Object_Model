c982042e3721966ebff369eb713ed869df7a7a42
Whaleer <1004707367@qq.com>
2024-11-21 22:01:07 +0800

    2024-11-21

---
+++ Chapter_1/02_point.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+class Point{
+public:
+    Point(float xval);
+    virtual ~Point();
+
+    float x() const;
+    static int point_count();
+
+protected:
+    virtual ostream& print(ostream& os) const;
+    float _x;
+    static int _point_count;
+};
+
+
