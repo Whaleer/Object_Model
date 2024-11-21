bd86014c0a999be5fa0eb7231aa8378ccd2a8382
Whaleer <1004707367@qq.com>
2024-11-21 20:09:47 +0800

    2024-11-21

--- Chapter_1/01_point3d.cpp
+++ Chapter_1/01_point3d.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
+using namespace std;
 
-
-class Point3d{
+class Point3d {
 public:
+    Point3d(float x = 0.0, float y = 0.0, float z = 0.0) : _x(x), _y(y), _z(z) {}
+
+    float x() const { return _x; }
+
+    float y() const { return _y; }
+
+    float z() const { return _z; }
 
 private:
     float _x;
     float _y;
     float _z;
-
 };
+
+inline ostream& operator<<(ostream& os, const Point3d &pt){
+    os << "(" << pt.x() << ", " << pt.y() << ", " << pt.z() << ")";
+    return os;
+}
+
+
+int main(){
+    Point3d pt1(1.0, 2.0, 3.0);
+    cout << pt1 << endl;
+    return 0;
+}
